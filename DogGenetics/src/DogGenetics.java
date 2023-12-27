47bb1fa1784863925b06dfab86062605595cb801
kelveloper <kelvinsaldana98@gmail.com>
2023-12-27 05:15:03 -0500

    Update on MAC 12/27/2023 on 5:14  AM

---
+++ DogGenetics/src/DogGenetics.java
@@ -0,0 +1,14 @@
+public class DogGenetics {
+    public static void main(String[] args) {
+        /*
+Ever heard of those places that you can mail in some of your dog’s hair, and they’ll send back a genetic analysis report to tell you what kind of dogs are in your most beloved pet’s ancestry?
+
+Well, we don’t know how to do that. But we DO know how to generate random numbers. And half the time, that’s good enough. Especially for the Internet.
+
+What You Should Do
+Program Name: DogGenetics
+Write a program that asks the user for the name of their dog, and then generates a fake DNA background report on the pet dog.
+It should assign a random percentage using five dog breeds. The total percentage should be 100%.
+         */
+    }
+}
--- LuckySevens/src/LuckySevens.java
+++ LuckySevens/src/LuckySevens.java
@@ -1,5 +1,35 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class LuckySevens {
     public static void main(String[] args) {
-        
+
+        int userMoney, dieOne, dieTwo, sum, countRolls = 0, maxAmount = 0, countRollsWhenMax = 0;
+
+        // need two dice that chooses randomly from 1-6
+        Random randomizer = new Random();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many dollars do you have? (dollars) ");
+        userMoney = sc.nextInt();
+
+        while (userMoney > 0) {
+            dieOne = randomizer.nextInt(6 - 1) + 1;
+            dieTwo = randomizer.nextInt(6 - 1) + 1;
+            sum = dieOne + dieTwo;
+            if (sum == 7) {
+                if (maxAmount < userMoney) {
+                    maxAmount = userMoney;
+                    countRollsWhenMax = countRolls;
+                }
+                userMoney += 4;
+            }
+            countRolls++;
+            userMoney--;
+        }
+
+        System.out.println("You are broke after " + countRolls + " rolls.");
+        System.out.println("You should have quit after " + countRollsWhenMax
+                + " rolls when you had $" + maxAmount);
+
     }
 }
